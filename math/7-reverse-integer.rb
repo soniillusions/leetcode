80a73410f85675bdf7e544f4fb632ba486542510
Alexey Maryshenkov <alekseycrosso@gmail.com>
2025-05-14 11:30:40 +0300

    ergergergreg

--- math/7-reverse-integer.rb
+++ math/7-reverse-integer.rb
@@ -5,9 +5,5 @@ def reverse(x)
   sign = x < 0 ? -1 : 1
   reverse = x.abs.to_s.reverse.to_i
   result = sign * reverse
-  if result > max || result < min
-    0
-  else
-    result
-  end
+  (result > max || result < min) ? 0 : result
 end
