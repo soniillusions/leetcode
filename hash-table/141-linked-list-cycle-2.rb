03cacdbe0fd3dbd1bd728901664b5dca929e5ad8
Alexey Maryshenkov <alekseycrosso@gmail.com>
2025-09-30 21:23:43 +0300

    dfkgjfdkgdfgfd

--- hash-table/141-linked-list-cycle-2.rb
+++ hash-table/141-linked-list-cycle-2.rb
@@ -2,10 +2,11 @@ def hasCycle(head)
   slow = head
   fast = head
 
-  while fast && fast.next 
+  while fast && fast.next
     slow = slow.next
     fast = fast.next.next
     return true if slow == fast
   end
-  return false
+
+  false
 end
