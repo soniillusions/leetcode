22a186c186a53c91599e67c39ed1a58d18af63bc
Alexey Maryshenkov <alekseycrosso@gmail.com>
2025-09-30 21:04:14 +0300

    fewfgwegweg

--- hash-table/141-linked-list-cycle-2.rb
+++ hash-table/141-linked-list-cycle-2.rb
@@ -1,14 +1,11 @@
 def hasCycle(head)
-  return false if head == nil
-
   slow = head
   fast = head
 
   while fast && fast.next 
     slow = slow.next
     fast = fast.next.next
-    break if fast.nil?
-    return true if slow.next == fast.next
+    return true if slow == fast
   end
   return false
 end
