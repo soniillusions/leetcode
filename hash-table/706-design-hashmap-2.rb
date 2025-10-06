31bd3a9d6946b4a4e3ef4fa4390ee967a5852bb7
Alexey Maryshenkov <alekseycrosso@gmail.com>
2025-10-06 12:07:17 +0300

    werwerewfewgfewgfwe

--- hash-table/706-design-hashmap-2.rb
+++ hash-table/706-design-hashmap-2.rb
@@ -1,24 +1,21 @@
-# frozen_string_literal: true
-
 class MyHashMap
   attr_accessor :buckets
 
-  BUCKETS_COUNT = 10000
-
+  BUCKETS_AMOUNT = 1000
   def initialize()
-    @buckets = Array.new(BUCKETS_COUNT) { [] }
+    @buckets = Array.new(BUCKETS_AMOUNT) { [] }
   end
 
   def put(key, value)
     bucket = bucket_for(key)
-    pair = bucket.find { |k, _v| k.eql?(key) }
+    pair = pair_for(key, bucket)
     pair ? pair[1] = value : bucket << [key, value]
   end
 
   def get(key)
     bucket = bucket_for(key)
-    pair = bucket.find { |k, _v| k.eql?(key) }
-    pair ? pair[1] : -1
+    pair = pair_for(key, bucket)
+    pair ? pair[1] : nil
   end
 
   def remove(key)
@@ -26,9 +23,16 @@ class MyHashMap
     bucket.reject! { |k, _v| k.eql?(key) }
   end
 
+  alias []= put
+  alias [] get
+
   private
 
   def bucket_for(key)
-    buckets[key.hash.abs % BUCKETS_COUNT]
+    buckets[key.hash.abs % BUCKETS_AMOUNT]
+  end
+
+  def pair_for(key, bucket)
+    bucket.find { |k, _v| k.eql?(key) }
   end
 end
